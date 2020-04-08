2fb4a658612893d6f06be4e8cc545b6b7ff47d60
Manel <mmpcp23@gmail.com>
2020-04-08 17:01:05 +0100

    Manel first Push

--- Circuit Slimes/Assets/Scripts/Puzzle/Pieces/Agents/Actions/Action.cs
+++ Circuit Slimes/Assets/Scripts/Puzzle/Pieces/Agents/Actions/Action.cs
@@ -16,5 +16,48 @@ namespace Puzzle.Actions
         {
             return true;
         }
+
+
+        #region === AUX Methods ===
+        protected static ArrayList CheckAdjacents(Agent agent)
+        {
+            ArrayList adjacents = new ArrayList();
+
+            var x = (int)agent.Coords.x;
+            var y = (int)agent.Coords.y;
+
+            // Right
+            if (agent.IsFree(x + 1, y))
+            {
+                adjacents.Add(true);
+            }
+            else adjacents.Add(false);
+
+            // Up
+            if (agent.IsFree(x, y - 1))
+            {
+                adjacents.Add(true);
+            }
+            else adjacents.Add(false);
+
+            // Down
+            if (agent.IsFree(x, y + 1))
+            {
+                adjacents.Add(true);
+            }
+            else adjacents.Add(false);
+
+            // Left
+            if (agent.IsFree(x - 1, y))
+            {
+                adjacents.Add(true);
+            }
+            else adjacents.Add(false);
+
+            if (adjacents.Count == 0) return null;
+
+            return adjacents;
+        }
+        #endregion
     }
 }
--- Circuit Slimes/Assets/Scripts/Puzzle/Pieces/Agents/Actions/SeekTarget.cs
+++ Circuit Slimes/Assets/Scripts/Puzzle/Pieces/Agents/Actions/SeekTarget.cs
@@ -6,25 +6,18 @@ using Puzzle.Pieces;
 
 namespace Puzzle.Actions
 {
-    public class CardinalMove : Action
+    public class SeekTarget : Action
     {
-        public LevelBoard.Directions Direction { get; private set; }
-        
-        public Vector2 TargetCoords { get; private set; }
-        public Vector3 TargetPosition { get; private set; }
+        public Piece Target { get; private set; }
 
-        public CardinalMove()
+        public SeekTarget()
         {
-            this.Direction = LevelBoard.Directions.None;
+            this.Target = null;
         }
 
-        public CardinalMove(LevelBoard.Directions dir, Vector2 coords, Vector3 position)
+        public SeekTarget(Piece target)
         {
-            this.Direction = dir;
-
-            this.TargetCoords = coords;
-
-            this.TargetPosition = position;
+            this.Target = target;
         }
 
         //
@@ -96,6 +89,50 @@ namespace Puzzle.Actions
             agent.transform.position = current;
 
             return false;
+        }
+
+        //
+        // - Aux Methods
+        //
+
+        private static ArrayList CheckAdjacents(Agent agent)
+        {
+            ArrayList adjacents = new ArrayList();
+
+            var x = (int) agent.Coords.x;
+            var y = (int) agent.Coords.y;
+
+            // Right
+            if (agent.IsFree(x + 1, y))
+            {
+                adjacents.Add(true);
+            }
+            else adjacents.Add(false);
+
+            // Up
+            if (agent.IsFree(x, y - 1))
+            {
+                adjacents.Add(true);
+            }
+            else adjacents.Add(false);
+
+            // Down
+            if (agent.IsFree(x, y + 1))
+            {
+                adjacents.Add(true);
+            }
+            else adjacents.Add(false);
+
+            // Left
+            if (agent.IsFree(x - 1, y))
+            {
+                adjacents.Add(true);
+            }
+            else adjacents.Add(false);
+
+            if (adjacents.Count == 0) return null;
+
+            return adjacents;
         }
     }
 }
