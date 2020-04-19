dceb8b82691e5a0c59207a74eb2d5a6dbadd65fc
fmbnicolau <fmbnicolau@hotmail.com>
2020-04-20 00:07:14 +0100

    Outline Thickness corrections + Selection WhiteList

--- Circuit Slimes/Assets/Scripts/SelectionManager/System/Selectors/RayCastBasedTagSelector.cs
+++ Circuit Slimes/Assets/Scripts/SelectionManager/System/Selectors/RayCastBasedTagSelector.cs
@@ -1,25 +1,37 @@
 ﻿using UnityEngine;
 using Puzzle;
+using System.Collections.Generic;
 
 public class RayCastBasedTagSelector : MonoBehaviour, ISelector
 {
+
     public string Tag = "Selectable";
-  
+
+    private SelectionManager Manager;
+
+    private List<Transform> WhiteList = null;
+
+    public void Start()
+    {
+        this.Manager = this.GetComponent<SelectionManager>();
+    }
+
     public Transform Check(Ray ray)
     {
+        this.WhiteList = Manager.WhiteList;
+
         if (Physics.Raycast(ray, out var hit))
         {
             var tr = hit.transform;
 
-            if(tr.CompareTag(this.Tag))
-            {
-                //Debug.Log("Bin");
-            }
+            //see if any object was hit
+            if (tr == null) return null;
 
-            if (tr != null && tr.CompareTag(this.Tag))
-            {
-                return tr;
-            }
+            //check if filter allows it
+            if (WhiteList != null && !WhiteList.Contains(tr)) return null;
+
+            //check if tag is correct
+            if (tr.CompareTag(this.Tag) ) return tr;
         }
 
         return null;
