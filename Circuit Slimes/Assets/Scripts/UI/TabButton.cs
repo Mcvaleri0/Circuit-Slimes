9becd94feb536ff1e8869864fbccf8604ea79f32
Josefin <josstrom@kth.se>
2020-04-28 10:36:04 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Circuit Slimes/Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(image))]
+
+public class TabButton : MonoBehaviour
+{
+    public TabGroup tabGroup;
+
+    public Image background;
+
+    void Start()
+    {
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Circuit Slimes/Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<TabButton> tabButtons;
+
+    public void Subscribe(TabButton button)
+    {
+        if (tabButtons == NULL)
+        {
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+
+    }
+}
