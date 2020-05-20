4b9fd88499ba7d657c77d777f226ab1f5068218f
Miguel <miguel.valerio@tecnico.ulisboa.pt>
2020-05-20 16:55:16 +0100

    wheel 4
    scrolling through the list works but dragging a item does not

--- Circuit Slimes/Assets/Scripts/UI-design/MyListBank.cs
+++ Circuit Slimes/Assets/Scripts/UI-design/MyListBank.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MyListBank : BaseListBank
 {
     private int[] _contents = {
-        0, 1, 2
+        0, 1, 2, 3
     };
 
     public override string GetListContent(int index)
