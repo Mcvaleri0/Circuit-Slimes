05e4b6e7148015ac880859983c4b3e8e73f74438
Miguel <miguel.valerio@tecnico.ulisboa.pt>
2020-05-20 16:48:32 +0100

    wheel with one item

--- Circuit Slimes/Assets/Scripts/UI-design/MyListBank.cs
+++ Circuit Slimes/Assets/Scripts/UI-design/MyListBank.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MyListBank : BaseListBank
 {
     private int[] _contents = {
-        1, 2, 3
+        0
     };
 
     public override string GetListContent(int index)
